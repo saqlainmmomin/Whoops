b3613d58b6de738a57f70f399d1a28d61400142c
Saqlain Momin <Saqlainmmomin@gmail.com>
2026-01-11 15:52:45 +0530

    Initial Commit

---
+++ Whoops/WhoopsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoopsApp.swift
+//  Whoops
+//
+//  Created by Saqlain Momin on 11/01/26.
+//
+
+import SwiftUI
+
+@main
+struct WhoopsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
